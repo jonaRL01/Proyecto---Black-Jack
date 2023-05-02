61f79a816caef9ffe0d0c1fcfbc97edd69c1ad82
Jonathan Reyes <jrlproduccion@gmail.com>
2023-05-02 14:25:49 -0600

    creando la funcionalidad del tuno de la máquina, creando las condiciones dependiendo el resultado de los puntos del jugardor, implementamos la funcionaldiad del bot´+on detener

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -13,8 +13,13 @@ let puntosJugador = 0,
     puntosComputadora = 0;
 
 // Referencias del HTML
-const btnPedir = document.querySelector('#btnPedir');
-const puntosHTML = document.querySelectorAll('small');
+const btnPedir             = document.querySelector('#btnPedir');
+const btnDetener           = document.querySelector('#btnDetener');
+
+const divCartasJugador     = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector('#computadora-cartas');
+
+const puntosHTML           = document.querySelectorAll('small');
 
 //Esta función crear un nuevo deck
 const crearDeck = () => {
@@ -55,9 +60,55 @@ const valorCarta = (carta) =>{
            : +valor 
 }
 
+// lógica de la computadora--> turno de la computadora
+const turnoComputadora = ( puntosMinimos) =>{
+
+    do{
+    const carta = pedirCarta();
+    puntosComputadora = puntosComputadora + valorCarta( carta );
+    puntosHTML[1].innerText = puntosComputadora;
+ 
+    const imgCarta  = document.createElement('img');
+    imgCarta.src = `assets/cartas/${ carta }.png`;
+    imgCarta.classList.add('carta');
+    divCartasComputadora.append(imgCarta);
+
+    if (puntosMinimos > 21) {
+        break;
+    }
+    }while( (puntosComputadora <= puntosMinimos) && (puntosMinimos <= 21) );
+}
+
+
 // Eventos
 btnPedir.addEventListener('click', () => {
    const carta = pedirCarta();
    puntosJugador = puntosJugador + valorCarta( carta );
    puntosHTML[0].innerText = puntosJugador;
+
+   const imgCarta  = document.createElement('img');
+   imgCarta.src = `assets/cartas/${ carta }.png`;
+   imgCarta.classList.add('carta');
+   divCartasJugador.append(imgCarta);
+
+   if (puntosJugador > 21) {
+        console.warn("lo siento, pasaste los 21 puntos");
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora( puntosJugador );
+        
+    }else if(puntosJugador === 21){
+        console.warn('21, genial');
+        btnDetener.disabled = true;
+        btnPedir.disabled = true;
+        turnoComputadora( puntosJugador );
+   }
+});
+
+btnDetener.addEventListener('click', ()=>{
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+
+    turnoComputadora( puntosJugador);
+
 });
